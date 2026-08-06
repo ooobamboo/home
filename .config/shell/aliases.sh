d4d7a1b8e645966141dbc95aef46aa4252391d51
aloires <aloires@tutamail.com>
2026-08-06 12:41:46 +0800

    feat: lf -> lfcd

--- .config/shell/aliases.sh
+++ .config/shell/aliases.sh
@@ -1,15 +1,12 @@
-# .config/zsh/aliases.zsh
-
 alias ls="ls --color=auto"
 
-alias rr="dbus-run-session -- river"
-alias st="exec /usr/bin/gamescope -W 2560 -H 1600 -r 165 -f -m 1 -e -- steam"
+alias lf="lfcd"
 
 alias yarn="yarn --use-yarnrc $XDG_CONFIG_HOME/yarn/config"
 alias ff="fastfetch"
 
-alias wshowkeys="wshowkeys -a bottom -F 'monospace 20'"
-
 alias dotfiles='/usr/bin/git --git-dir="$HOME/doc/.dotfiles/" --work-tree="$HOME"'
 
 alias adb='HOME="${XDG_STATE_HOME:-$HOME/.local/state}/adb" adb'
+
+alias update='sudo xbps-install -Syu; flatpak update'
