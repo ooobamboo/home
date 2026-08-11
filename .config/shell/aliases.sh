5266b4181aef8fc6851e4e8f2380105d54c1796e
aloires <aloires@tutamail.com>
2026-08-11 12:28:58 +0800

    chore: remove fastfetch

--- .config/shell/aliases.sh
+++ .config/shell/aliases.sh
@@ -3,10 +3,7 @@ alias ls="ls --color=auto"
 alias lf="lfcd"
 
 alias yarn="yarn --use-yarnrc $XDG_CONFIG_HOME/yarn/config"
-alias ff="fastfetch"
 
-alias dotfiles='/usr/bin/git --git-dir="$HOME/doc/.dotfiles/" --work-tree="$HOME"'
+alias dotfiles='/usr/bin/git --git-dir="$XDG_DOCUMENTS_DIR/.dotfiles/" --work-tree="$HOME"'
 
-alias adb='HOME="${XDG_STATE_HOME:-$HOME/.local/state}/adb" adb'
-
-alias update='sudo xbps-install -Syu; flatpak update'
+alias adb='HOME="$XDG_STATE_HOME/adb" adb'
