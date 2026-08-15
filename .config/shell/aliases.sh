e3b53fabfbbad30d9969ad61643f7e7668fd179a
aloires <aloires@tutamail.com>
2026-08-15 13:51:02 +0800

    fix: fix .config/shell/aliases.sh

--- .config/shell/aliases.sh
+++ .config/shell/aliases.sh
@@ -2,7 +2,7 @@ alias ls="ls --color=auto"
 
 alias lf="lfcd; printf '\033[0m'"
 
-alias wget --hsts-file="$XDG_STATE_HOME/wget-hsts"
+alias wget='wget --hsts-file="$XDG_STATE_HOME/wget-hsts"'
 
 alias yarn="yarn --use-yarnrc $XDG_CONFIG_HOME/yarn/config"
 
